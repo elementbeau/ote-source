217d518cdf4d51138c682efb50512be791b77add
Scott Harris <scotth519@gmail.com>
2026-01-25 21:51:05 -0800

    removed the first and last name of the upload page

--- frontend/src/routes/user_upload.tsx
+++ frontend/src/routes/user_upload.tsx
@@ -25,33 +25,6 @@ function RouteComponent() {
         {/** test section for input data (looks ugly) */}
         <main className="flex flex-wrap justify-center-safe items-center-safe bg-white">
             <form className="w-full max-w-lg flex flex-wrap justify-center-safe items-center-safe">
-                {/** Naming Block */}
-                <div className="flex flex-wrap -mx-3 mg-6 my-3">
-                    {/** First Name Text Box */}
-                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" 
-                            htmlFor="grid-first-name">
-                            First Name
-                        </label>
-                    <input className="appearance-non block w-full bg-gray-200 text-gray-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg:white" 
-                        id="grid-first-name" 
-                        type="text" 
-                        placeholder="First Name">
-                    </input>
-                    </div>
-                    {/** Last Name Text Box */}
-                    <div className="w-full md:w-1/2 px-3">
-                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" 
-                            htmlFor="grid-last-name">
-                            Last Name
-                        </label>
-                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
-                        id="grid-last-name" 
-                        type="text" 
-                        placeholder="Last Name">
-                        </input>
-                    </div>
-                </div>
                 {/** Location / Campus Block */}
                 <div className="flex flex-wrap w-full justify-center-safe items-center-safe">
                     <div className="w-full mb-3 mx-18 my-3">
@@ -242,9 +215,9 @@ function RouteComponent() {
                     </button>
                     <button
                     type="submit"
-                    className="rounded-md bg-indigo-500 px-15 py-2 text-m font-semibold text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                    className="rounded-md bg-blue-500 px-15 py-2 text-m font-semibold text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
                     >
-                    Save
+                    Publish
                     </button>
                 </div>
             </form>
