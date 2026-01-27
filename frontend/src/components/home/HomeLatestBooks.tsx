67432e84103eb4126cf83ae6833af6826fc4184c
Scott Harris <scotth519@gmail.com>
2026-01-26 16:51:37 -0800

    Updated HomeLatestBooks and HomePopularBooks to have added bookHeight parameter, and made it flex so it scales with page resizing.

--- frontend/src/components/home/HomeLatestBooks.tsx
+++ frontend/src/components/home/HomeLatestBooks.tsx
@@ -2,12 +2,22 @@ import BookGridSection from "../explore/BookGridSection";
 
 export default function HomeLatestBooks() {
   return (
-    <BookGridSection
-      backgroundClass="bg-gray-400"
-      heightClass="h-[600px]"
-      title="Book"
-      count={4}
-      columns={4}
-    />
+    <>
+      <section className="h-auto justify-items-center-safe justify-center">
+          <div className="mx-auto p-6 w-auto px-4 flex flex-wrap justify-items-center-safe justify-center-safe gap-8">
+            <h1 className="text-white font-bold mt-3 text-5xl">
+              Latest Additions
+            </h1>
+            <BookGridSection
+              backgroundClass="bg-gray-400"
+              heightClass="h-auto"
+              title="Book"
+              count={4}
+              columns={4}
+              bookHeight="w-70"
+            />
+          </div>
+      </section>
+    </>
   );
 }
